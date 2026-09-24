8c79d9a6061eae884212868f531545cad0dd0e8d
kailashmishra-hub <kailasa.kailash@gmail.com>
2026-09-24 12:27:41 +0530

    Commited

--- src/main/java/com/api/utils/TestContext.java
+++ src/main/java/com/api/utils/TestContext.java
@@ -17,7 +17,8 @@ public class TestContext {
 	public Map<String, Object> session = new HashMap<String, Object>();
 	private static final String CONTENT_TYPE = PropertiesFile.getProperty("content.type");
 	
-	public RequestSpecification requestSetup() {	
+	public RequestSpecification requestSetup() {
+		System.out.println("Base URL: "+PropertiesFile.getProperty("baseURL"));
 		RestAssured.reset();
 		Options options = Options.builder().logStacktrace().build();
 		RestAssuredConfig config = CurlRestAssuredConfigFactory.createConfig(options); 
